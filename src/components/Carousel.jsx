b9a8625b41f5de2fbbbce20327f8993243244ecd
DYLAN OTINA <dylan.otina@gmail.com>
2022-09-27 13:17:24 -0700

    adding spinners to wait load for carousels

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Flickity from 'react-flickity-component'
+import MoonLoader from 'react-spinners/MoonLoader'
 import "../styles/flickity.css"
 function Carousel({arrayImages}) {
+
+const [loading, setLoading] = useState(false)
+
+
+useEffect( () => {
+  setLoading(true)
+  setTimeout(() => setLoading(false), 2000)
+}, [])
+
   return (
-    <Flickity options={ {"wrapAround" : true, "autoPlay" : true, "prevNextButtons" : false, "pageDots" : false} }>
-        {arrayImages?.map((img, index) => <img src={img} alt='project' key={index}/>  )}
+    loading ? 
+      <MoonLoader color='#5366aa'/> :
+      <Flickity options={ {"wrapAround" : true, "autoPlay" : true, "prevNextButtons" : false, "pageDots" : false} }>
+        { arrayImages?.map((img, index) => <img src={img} alt='project' key={index}/>  )}
     </Flickity>
+  
   )
 }
 
